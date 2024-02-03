bee70648c818fb8833b1b2e9c41caeb31411c878
anushka-pande <2022bit042@sggs.ac.in>
2024-02-03 14:35:38 +0530

    assignment 1 completed

--- ProgrammingAssignment1.java
+++ ProgrammingAssignment1.java
@@ -6,39 +6,42 @@ class SY2022bit042 {
 		for(String registrationNo : registrations) {
 			validRegCount += valid.getValidRegistrationsCount(registrationNo);
 		}
+		System.out.println(validRegCount);
 	}
 	public int getValidRegistrationsCount(String registrationNo) {
 		int count=0;
-			if(registrationNo.length() = 10) {
-				String first4chars = registrationNo.substring(0,3);
-				String last3chars = regitrationNo.substring(7,9);
+			if(registrationNo.length() == 10) {
+				int year= Integer.parseInt(registrationNo.substring(0,4));
 				
-				if(isNumeric(first4hcars) && isNumeric(last3chars)) {
-					String middle3chars = registrationNo.substring(4,6);
+				if(year >= 2000 && year <= 2024) {
+					String dept = registrationNo.substring(4,7);
+					String sequence = registrationNo.substring(7,10);
 					
-					if(isAlphabetic(middle3chars)){
+					if("bit".equals(dept) && isNumeric(sequence)) {
 						count=1;
 					}
-				}	
+					else if("bcs".equals(dept) && isNumeric(sequence)) {
+						count=1;
+					}
+					else if("bch".equals(dept) && isNumeric(sequence)) {
+						count=1;
+					}
+					else if("bme".equals(dept) && isNumeric(sequence)) {
+						count=1;
+					}
+					else {
+						count=0;
+					}
+				}
 			}
 		return count;
 	}
-	public int isNumeric(String string) {
-		int count=0;
+	public boolean isNumeric(String string) {
 		for(char c : string.toCharArray()) {
-			if(Character.isDigit(c)) {
-				count++;
+			if(!Character.isDigit(c)) {
+				return false;
 			}
 		}
-		return count;	
-	}
-	public int isAlphabetic(String string) {
-		int count=0;
-		for(char c : string.toCharArray()) {
-			if(Character.isLetter(c)) {
-				count++;
-			}
-		}
-		return count;	
+		return true;	
 	}
 }
